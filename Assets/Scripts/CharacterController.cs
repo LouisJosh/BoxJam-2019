9500e2f6b3c0030fb93d289f2a12555893b85c92
LouisJosh <40745585+LouisJosh@users.noreply.github.com>
2019-05-22 10:09:55 +1000

    added movement to the player

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+	public Vector2 velocity;
+	public float speed;
+	public float walkAcceleration;
+	public float groundDeceleration;
+	public float speedMax;
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.Translate(velocity * Time.deltaTime);
+
+		float moveInput = Input.GetAxisRaw("Horizontal");
+		if (moveInput != 0)
+		{
+			velocity.x = Mathf.MoveTowards(velocity.x, speed * moveInput, walkAcceleration * Time.deltaTime);
+		}
+		else
+		{
+			velocity.x = Mathf.MoveTowards(velocity.x, 0, groundDeceleration * Time.deltaTime);
+		}
+		if (velocity.x >= speedMax)
+		{
+			velocity.x = speedMax;
+		}
+		else { }
+	}
+}
